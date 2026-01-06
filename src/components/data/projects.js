cc403c8fcdcb7338fcc3f8e462eae191966b4a13
Otega Otite <otiteotega@gmail.com>
2026-01-06 20:54:19 +0100

    build folder structure, provide dummy projects in data folder

---
+++ src/components/data/projects.js
@@ -0,0 +1,81 @@
+export const projects = [
+    {
+        id: 1,
+        title: 'E-Commerce Platform',
+        description: 'Full-stack online shopping application with real-time inventory management, secure payment...',
+        image: '/images/projects/projects3.png',
+        category: 'Full Stack',
+        technologies: ['React', 'Node.js', 'MongoDB', 'Stripe'],
+        metrics: '40% increase in conversion',
+        demoUrl: 'https://demourl.com/projects',
+        githuburl: 'https://github.com/Tega96/portfolio'
+    },
+    {
+        id: 3,
+        title: 'E-Commerce Platform',
+        description: 'Full-stack online shopping application with real-time inventory management, secure payment...',
+        image: '/images/projects/projects3.png',
+        category: 'Full Stack',
+        technologies: ['React', 'Node.js', 'MongoDB', 'Stripe'],
+        metrics: '40% increase in conversion',
+        demoUrl: 'https://demourl.com/projects',
+        githuburl: 'https://github.com/Tega96/portfolio'
+    },
+    {
+        id: 4,
+        title: 'E-Commerce Platform',
+        description: 'Full-stack online shopping application with real-time inventory management, secure payment...',
+        image: '/images/projects/projects3.png',
+        category: 'Full Stack',
+        technologies: ['React', 'Node.js', 'MongoDB', 'Stripe'],
+        metrics: '40% increase in conversion',
+        demoUrl: 'https://demourl.com/projects',
+        githuburl: 'https://github.com/Tega96/portfolio'
+    },
+    {
+        id: 5,
+        title: 'E-Commerce Platform',
+        description: 'Full-stack online shopping application with real-time inventory management, secure payment...',
+        image: '/images/projects/projects3.png',
+        category: 'Full Stack',
+        technologies: ['React', 'Node.js', 'MongoDB', 'Stripe'],
+        metrics: '40% increase in conversion',
+        demoUrl: 'https://demourl.com/projects',
+        githuburl: 'https://github.com/Tega96/portfolio'
+    },
+    {
+        id: 6,
+        title: 'E-Commerce Platform',
+        description: 'Full-stack online shopping application with real-time inventory management, secure payment...',
+        image: '/images/projects/projects3.png',
+        category: 'Full Stack',
+        technologies: ['React', 'Node.js', 'MongoDB', 'Stripe'],
+        metrics: '40% increase in conversion',
+        demoUrl: 'https://demourl.com/projects',
+        githuburl: 'https://github.com/Tega96/portfolio'
+    },
+    {
+        id: 7,
+        title: 'E-Commerce Platform',
+        description: 'Full-stack online shopping application with real-time inventory management, secure payment...',
+        image: '/images/projects/projects3.png',
+        category: 'Full Stack',
+        technologies: ['React', 'Node.js', 'MongoDB', 'Stripe'],
+        metrics: '40% increase in conversion',
+        demoUrl: 'https://demourl.com/projects',
+        githuburl: 'https://github.com/Tega96/portfolio'
+    },
+    {
+        id: 2,
+        title: 'Component Library',
+        description: 'Comprehensive React component library with 50+ customizable components, built with access...',
+        image: '/images/projects/project1.png',
+        category: 'Web Design',
+        technologies: ['Figma', 'Html', 'CSS'],
+        metrics: '1k+ npm downloads',
+        demoUrl: 'https://demourl.com',
+        githuburl: 'https://github.com'
+    }
+];
+
+export const categories = ['All', 'Web Apps', 'Full Stack', 'Web Design'];
