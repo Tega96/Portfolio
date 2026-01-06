2495c12e0da19582d8d23b7e36fcad97be4737f3
Otega Otite <otiteotega@gmail.com>
2026-01-06 21:58:21 +0100

    scroll hooks

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react'
+import { NAV_LINKS, PERSONAL_INFO } from "../utils/constants";
+import {Code, Menu, X} from 'lucide-react'
+
+
+const Navbar = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+    const activeSection = useScrollSpy(NAV_LINKS.map(link => link.id));
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const handleNavClick = (sectionId) => {
+        scrollToSection(sectionId);
+        setIsMenuOpen(false);
+    };
+
+    return (
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/components/utils/constants.js
@@ -0,0 +1,40 @@
+export const PERSONAL_INFO = {
+    name: 'Otega Otite',
+    title: 'Software Engineer',
+    email: 'otiteotega@gmail.com',
+    location: 'Lekki, Lagos, Nigeria.',
+    tagline: 'Crafting seamless digital experience with modern web technologies',
+    resume: '/resume.pdf',
+    bio: [
+        "I'm a passionate Software developer with over 3 years of experience building ...",
+        "My expertise spans the entire frontend ecosystem, from React and Next.js...",
+        "When I'm not coding, you'll find me contributing to open-source projects or reading books"
+    ]
+};
+
+export const SOCIAL_LINKS = {
+    github: 'https://github.com/Tega96',
+    linkedin: 'https://linkedin.com/in/otega-otite',
+    dribble: 'https://dribbble.com'
+};
+
+export const STATS = [
+    { lable: 'Years Experience', value: '3+' },
+    { lable: 'Projects Completed', value: '50+' },
+    { lable: 'Technologies', value: '10+' },
+    { lable: 'Client Satisfaction', value: '98%' },
+];
+
+export const ABOUT_STATS = [
+    { label: 'Happy Clients', value: '45+' },
+    { label: 'Code Commits', value: '2.5k+' },
+    { label: 'GitHub Stars', value: '5+' }
+];
+
+export const NAV_LINKS = [
+    { id: 'about', label: 'About' },
+    { id: 'skills', label: 'Skills' },
+    { id: 'projects', label: 'Projects' },
+    { id: 'services', label: 'Services' },
+    { id: 'contact', label: 'Contact' },
+];
